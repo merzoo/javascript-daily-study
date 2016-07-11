a3bbf3ebd9f67c0e20b3d585f0674f03e60c57b1
郭晋良 <gjl33431@ly.com>
2016-07-11 18:06:48 +0800

    prictice

---
+++ TestCase/function.name.js
@@ -0,0 +1,16 @@
+var canFly = function(){
+	return true;
+}
+console.log(canFly.name); // name == ""
+
+window.isDeadly = function(){
+	return true;
+}
+console.log(isDeadly.name); // name == ""
+
+
+window.faceName = function innerName(){
+	return true;
+};
+
+console.log(faceName.name);//innerName;
