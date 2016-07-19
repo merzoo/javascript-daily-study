b2978668d102f06e43996755f6c3a947b2efc1b3
郭晋良 <gjl33431@ly.com>
2016-07-19 17:19:30 +0800

    addAll

---
+++ Baidu IFE2015/task0002/util.js
@@ -0,0 +1,13 @@
+//判断arr是不是一个数组，返回一个布尔值
+function isArray(arr){
+    //your code
+    return Array.isArray(arr);
+}
+
+//判断一个fn是否为函数，返回一个bool值
+function isFunction(fn){
+    //your code
+    
+}
+
+console.log(isArray([1,2,3]));
