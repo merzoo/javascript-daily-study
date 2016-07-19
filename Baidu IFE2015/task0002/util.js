c3d656062667a6b6e381c9edcb86f3a0ea613569
郭晋良 <gjl33431@ly.com>
2016-07-19 18:42:00 +0800

    clone

--- Baidu IFE2015/task0002/util.js
+++ Baidu IFE2015/task0002/util.js
@@ -1,13 +1,29 @@
 //判断arr是不是一个数组，返回一个布尔值
 function isArray(arr){
     //your code
-    return Array.isArray(arr);
+    return Object.prototype.toString.call(arr) === "[object Array]";
 }
 
 //判断一个fn是否为函数，返回一个bool值
 function isFunction(fn){
     //your code
-    
+    return Object.prototype.toString.call(fn) === "[object Function]";
 }
 
-console.log(isArray([1,2,3]));
+function cloneObject(src){
+    //your code
+    var newSrc = showType(src);
+    switch (newSrc){
+        case "String":
+            console.log(1);
+            break;
+        case "Function":
+            console.log(2);
+            break;
+    }
+
+}
+
+function showType(str){
+    return Object.prototype.toString.call(str).slice(8,-1);
+}
