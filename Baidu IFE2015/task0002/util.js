898e20543cee6f9881412cf5ec21a8e9419dda23
郭晋良 <gjl33431@ly.com>
2016-07-20 18:36:22 +0800

    1

--- Baidu IFE2015/task0002/util.js
+++ Baidu IFE2015/task0002/util.js
@@ -10,20 +10,61 @@ function isFunction(fn){
     return Object.prototype.toString.call(fn) === "[object Function]";
 }
 
-function cloneObject(src){
-    //your code
-    var newSrc = showType(src);
-    switch (newSrc){
-        case "String":
-            console.log(1);
-            break;
-        case "Function":
-            console.log(2);
-            break;
-    }
-
+//判断一个obj是否为对象，返回一个bool值
+function isPlain(obj){
+    return Object.prototype.toString.call(obj) === "[object Object]";
 }
 
-function showType(str){
-    return Object.prototype.toString.call(str).slice(8,-1);
+function cloneObject(src){
+   var result = src, i, len;
+    if(!src
+        || src instanceof Number
+        || src instanceof Boolean
+        || src instanceof String ){
+        result = src;
+    }else if(isArray(src)){
+        result = [];
+        for(i = 0, len = src.length; i < len; i++){
+            result[i] = cloneObject(src[i]);
+        }
+    }else if(isPlain(src)){
+        result = {};
+        for(var item in src){
+            if(src.hasOwnProperty(item)){
+                result[item] = cloneObject(src[item]);
+            }
+        }
+    }
+    return result;
+}
+/**
+ * 测试用例：
+ * */
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+console.log(tarObj);
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hi";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
+
+
+//数组去重
+function uniArray(arr){
+    //your code
+    var newArr = [];
+    for(var i = 0, len = arr.length; i < len; i++){
+        if(arr[i] == )
+    }
 }
