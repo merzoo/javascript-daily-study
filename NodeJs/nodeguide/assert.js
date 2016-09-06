6a2556df07374baebe7a36e90eba5ac68c8630ea
郭晋良 <gjl33431@ly.com>
2016-09-06 16:40:15 +0800

    124

---
+++ NodeJs/nodeguide/assert.js
@@ -0,0 +1,8 @@
+/**
+ * Created by gjl33431 on 2016/8/26.
+ */
+const assert = require('assert');
+
+assert(true);  // OK
+
+assert(false);
---
+++ TestCase/cart/cart.js
@@ -0,0 +1,25 @@
+/*产品对象*/
+
+function Product(){
+    //产品名称
+    this.name = name || flower;
+    //价格
+    this.price = price || 0;
+    //图片
+    this.image = "";
+}
+
+Product.prototype = {
+    bindDom: function(){
+        var str = '';
+        str+='';
+        str+='';
+        str+='';
+        str+='';
+        str+='';
+        str+='';
+
+    }
+};
+
+window.onload =
